862d08a28e6a99611ae2f5a13c44a0d283b04714
Fernando <farriagadal94@gmail.com>
2023-02-01 00:22:54 -0300

    change api 18

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://spacecontrol.app/api'
+  baseURL: 'http://space-load-balancer-1672437956.us-east-2.elb.amazonaws.com'
   // baseURL: 'https://13.58.222.181:3000/api'
   // baseURL: 'http://127.0.0.1:3000/api',
 })
