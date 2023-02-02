1a81aa8bff96b1671c4d3127c68832e92b957eff
Fernando <farriagadal94@gmail.com>
2023-02-01 21:22:51 -0300

    change https 19

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://space-load-balancer-1672437956.us-east-2.elb.amazonaws.com'
-  // baseURL: 'https://13.58.222.181:3000/api'
+  baseURL: 'https://13.58.222.181:3000/api'
   // baseURL: 'http://127.0.0.1:3000/api',
 })
