b1ddd5e7111057f1b53bdb678cc64cc5ac13ee0b
Fernando <farriagadal94@gmail.com>
2023-02-10 15:22:52 -0300

    change animation routes 34

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import { AnimatedSwitch } from 'react-router-transition'
 
+// import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import { ThemeProvider } from '@material-ui/core'
 import theme from './theme'
 
@@ -17,26 +18,27 @@ import Layout from './layout/Layout'
 import './assets/scss/app.scss'
 
 const App = () => {
+  // const location = useLocation()
+
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <AnimatedSwitch
-            atEnter={{ opacity: 0 }}
-            atLeave={{ opacity: 0 }}
-            atActive={{ opacity: 1 }}
-            className="switch-wrapper"
-          >
-            <Route exact path="/" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/confirm-email" component={ConfirmEmail} />
-            <Route exact path="/verify-email" component={EmailVerified} />
-            <RouteWrapper path="/profile" component={Profile} />
-            <RouteWrapper path="/rooms" component={Rooms} />
-          </AnimatedSwitch>
-        </div>
-      </ThemeProvider>
-    </BrowserRouter>
+  // eslint-disable-next-line react/jsx-filename-extension
+  <ThemeProvider theme={theme}>
+    <div className="App">
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className="switch-wrapper"
+      >
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/confirm-email" component={ConfirmEmail} />
+        <Route exact path="/verify-email" component={EmailVerified} />
+        <RouteWrapper path="/profile" component={Profile} />
+        <RouteWrapper path="/rooms" component={Rooms} />
+      </AnimatedSwitch>
+    </div>
+  </ThemeProvider>
   )
 }
 
