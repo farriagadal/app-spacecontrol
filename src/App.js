8e42054b1d9a16ddcc3145d90393dd9bd9462a12
Fernando <farriagadal94@gmail.com>
2023-02-19 12:22:51 -0300

    config autoprefix css 49

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import { Route } from 'react-router-dom'
-
-import { ThemeProvider } from '@material-ui/core'
-import theme from './theme'
-
 import Login from './views/Login/Login';
-import Register from './views/Register/Register';
-import Profile from './views/Profile/Profile';
-
-import './assets/scss/app.scss'
+import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <Login />
+    </div>
   );
 }
 
--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -5,7 +5,7 @@ import './InputText.scss';
 const InputText = props => {
   return (
     <div className="InputText">
-      <div className="InputText__label">{ props.label }</div>
+      <div>{ props.label }</div>
       <input type={ props.type } />
     </div>
   );
