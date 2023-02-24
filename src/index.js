d9eabacd1be02070faaffd7d8d155f8fef140b0e
Fernando <farriagadal94@gmail.com>
2023-02-24 18:22:51 -0300

    change animation routes 34

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 
-import App from './App'
+import './index.css';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   // eslint-disable-next-line react/jsx-filename-extension
-  <App />,
-  document.getElementById('root')
-)
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
